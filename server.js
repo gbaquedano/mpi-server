4bc15d4f83355a44073495707c606a4a5bee9f06
gbaquedano <gbaquedano@standardprofil.com>
2015-07-05 20:12:36 +0200

    Add .gitignore file

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
-var io = require('socket.io')(3012);
+console.log('Server started...');
+// Express y Socket.IO
+var app = require('express')();
+var serveStatic = require('serve-static')
+var server = require('http').Server(app);
+// Os
+var io = require('socket.io')(server);
+var os = require('os');
 
 io.on('connection',function(d){
         console.log('Test:' + d);
@@ -7,4 +14,12 @@ io.on('connection',function(d){
         });
 });
 
-console.log('Server started...');
+server.listen(80);
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+app.use('/scripts',serveStatic(__dirname + '/scripts'))
+app.use('/css',serveStatic(__dirname + '/css'))
+app.use('/fonts',serveStatic(__dirname + '/fonts'))
+app.use('/img',serveStatic(__dirname + '/img'))
